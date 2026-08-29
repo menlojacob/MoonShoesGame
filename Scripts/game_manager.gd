0c9e1cc1d7aec665db972b494b76986dea1a5cbb
Osome55 <oscarlearymalone@gmail.com>
2026-08-29 15:32:07 -0400

    Making global game_manager script

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,8 +1,11 @@
 extends Node
 
-var score: int = 0
-@onready var score_label: Label = $ScoreLabel
 
-func add_point():
-	score += 1
-	score_label.text = "You got " + str(score) + " out of 20 coins!"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
