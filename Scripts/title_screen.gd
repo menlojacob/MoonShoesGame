81dba268affab05b225950396a04d103d32aaf5e
Osome55 <oscarlearymalone@gmail.com>
2026-09-01 21:09:11 -0400

    Splash screen only plays once!

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,15 +1,19 @@
 extends Node2D
+@onready var splash_screen: Control = $SplashScreen
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	splash_screen.visible = not GameManager.splash_screen_shown
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _on_splash_screen_fade_out() -> void:
+	GameManager.splash_screen_shown = true
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_one.tscn")
