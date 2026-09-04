618b5b568f965b9f3f0eb52a8e3a2f5093a3e8ad
Osome55 <oscarlearymalone@gmail.com>
2026-09-04 10:44:49 -0400

    Levels disabled if not unlocked

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,6 +1,12 @@
 extends Node
-@onready var level_one: PackedScene = preload("res://Scenes/level_one.tscn")
-@onready var splash_screen_shown = false
+var splash_screen_shown = false
+## Level completion flags
+var level_one_beat = false
+var level_two_beat = false
+var level_three_beat = false
+var level_four_beat = false
+var level_five_beat = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
