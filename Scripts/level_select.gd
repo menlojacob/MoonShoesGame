ce5f96545f87c0bb7e01759f2287f9da0d4b26cb
Osome55 <oscarlearymalone@gmail.com>
2026-09-04 10:54:54 -0400

    Added back button

--- Scripts/level_select.gd
+++ Scripts/level_select.gd
@@ -47,3 +47,7 @@ func _on_level_4_pressed() -> void:
 
 func _on_level_5_pressed() -> void:
 	get_tree().change_scene_to_packed(level_five)
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
