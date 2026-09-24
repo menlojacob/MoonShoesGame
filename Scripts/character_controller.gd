f48a08800a76b33cc88eb28f5afdef860dcc23a8
ultrakielb1 <cringewhale@gmail.com>
2026-09-23 22:31:19 -0400

    Revert "strange new abilities"
    This reverts commit 3eac2efa3469e8f5628160ee41470898eb718c1c, reversing
    changes made to 4b6639498b1e8a3a4eeaab08a3355d465b730f8d.

--- Scripts/character_controller.gd
+++ Scripts/character_controller.gd
@@ -1,82 +1,42 @@
 extends Node
 
 const SPEED = 110.0
-const JUMP_CANCEL_FACTOR = 0.5 #when you do a short jump, it's JUMP_VELOCITY * JUMP_CANCEL_FACTOR
-const JUMP_HEIGHT = 46
-const COYOTE_TIME = 0.15 * 1000 #0.15 seconds, the 1000 is for calculation reasons
+const JUMP_VELOCITY = -300.0
 
 @onready var character = get_parent()
 @onready var collisionArea = character.get_node("EnemyCollision")
 @onready var lastSafePosition = character.global_position
-@onready var sprite: AnimatedSprite2D = character.get_node("Sprite")
-@onready var characterCollision = character.get_node("CollisionShape2D")
+@onready var sprite: AnimatedSprite2D = $"../Sprite"
 
 var lastJumpedOnEnemyId = 0
-var jumping = false # for the jump animation so it doesnt get overwritten
-var movementLocked = 0 #if above 0, A and D keys will not influence horizontal movement
-var direction = null
-var lastValidDirection = 1
-var busy = false #dashing, ground pounding, etc.
-var jumpReleased = false
-var invulnerable = 0
-var horizontalSpeedModifier = 1
-var gravityModifier = 1
-var lastOnGroundTime = 0
-
-signal touchedEnemy
-signal jumpedOnEnemy
-
-func jump(height = JUMP_HEIGHT, variableHeight = true):
-	#calculate force needed to reach height
-	var gravity = (character.get_gravity().y * gravityModifier)
-	var impulse = -sqrt(2 * gravity * height)
-	character.velocity.y = impulse
-	jumping = true
-	jumpReleased = not variableHeight
-	
-	play_jumping_animation()
-
-func jumpOffEnemy(enemyBody, enemyController):
-	#we try to make the player bounce to the same height every time
-	#regardless of initial height when they hit the enemy.
-	var bounceHeight = JUMP_HEIGHT
-	var enemyCollisionShape = enemyBody.get_node_or_null("CollisionShape2D")
-	if enemyCollisionShape:
-		var topOfEnemyHeight = enemyBody.global_position.y - (enemyCollisionShape.shape.size.y/2)
-		var bottomOfPlayerHeight = character.global_position.y + (characterCollision.shape.size.y/2)
-		var difference = bottomOfPlayerHeight - topOfEnemyHeight
-		bounceHeight += difference
-	
-	if not isMovementLocked():
-		jump(bounceHeight, false)
-					
-	enemyController.jumped_on()
-	lastJumpedOnEnemyId = enemyBody.get_instance_id()
-	jumpedOnEnemy.emit()
+var jumping = false # for the jump animation so it doesnt get overwritten\
+var bouncing = false
 
 func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not character.is_on_floor():
+		character.velocity += character.get_gravity() * delta
+	else:
+		jumping = false
+		bouncing = false
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and character.is_on_floor():
+		jumping = true
+		character.velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+		
+	# Apply direction
+	if direction:
+		character.velocity.x = direction * SPEED
+	else:
+		character.velocity.x = move_toward(character.velocity.x, 0, SPEED)
+		
 	if character.is_on_floor():
 		lastJumpedOnEnemyId = 0
-		jumping = false
-		
-		var currentTime = Time.get_ticks_msec()
-		lastOnGroundTime = currentTime
-	
-	if not isMovementLocked():
-		# Add the gravity.
-		if not character.is_on_floor():
-			character.velocity += character.get_gravity() * gravityModifier * delta
-
-		# Handle jump.
-		if Input.is_action_just_pressed("jump") and isOnGround() and (not jumping):
-			jump()
-		
-		direction = Input.get_axis("move_left", "move_right")
-		if direction:
-			character.velocity.x = direction * SPEED * horizontalSpeedModifier
-			lastValidDirection = direction
-		else:
-			character.velocity.x = move_toward(character.velocity.x, 0, SPEED)
 		
 	handle_animations(direction)
 	
@@ -102,18 +62,19 @@ func _physics_process(delta: float) -> void:
 			
 			if (not isMovingDownward) or (isBelowEnemy):
 				# get hit
-				if (not isInvulnerable()) and body.get_instance_id() != lastJumpedOnEnemyId:
+				if body.get_instance_id() != lastJumpedOnEnemyId:
 					respawn()
 			else:
 				if isMovingDownward:
 					# bounce
-					jumpOffEnemy(body, enemyController)
-			
-			touchedEnemy.emit(body, enemyController)
-	
-	if (not Input.is_action_pressed("jump")) and (character.velocity.y > -275) and (character.velocity.y < 0) and (not jumpReleased):
-		jumpReleased = true
-		character.velocity.y *= JUMP_CANCEL_FACTOR
+					jumping = false # reset the jump animation
+					bouncing = false # reset the jump animation
+					handle_animations(direction)
+					bouncing = true
+					handle_animations(direction)
+					character.velocity.y = JUMP_VELOCITY
+					enemyController.jumped_on()
+					lastJumpedOnEnemyId = body.get_instance_id()
 	
 	character.move_and_slide()
 
@@ -122,45 +83,6 @@ func respawn():
 	character.get_node("CollisionShape2D").set_deferred("disabled", false)
 	get_tree().call_group("EnemyController","respawn")
 	
-func isMovementLocked():
-	return movementLocked > 0
-	
-func lockMovement():
-	movementLocked += 1
-	
-func unlockMovement():
-	movementLocked -= 1
-
-func getDirection():
-	return direction
-	
-func getLastValidDirection():
-	return lastValidDirection
-
-func isBusy():
-	return busy
-	
-func setBusy(isBusy):
-	busy = isBusy
-
-func setInvulnerable(isInvulnerable : bool):
-	if isInvulnerable:
-		invulnerable += 1
-	else:
-		invulnerable -= 1
-		
-func isInvulnerable():
-	return invulnerable > 0
-
-func setHorizontalSpeedModifier(modifier):
-	horizontalSpeedModifier = modifier
-	
-func setGravityModifier(modifier):
-	gravityModifier = modifier
-
-func isOnGround():
-	return (Time.get_ticks_msec() - lastOnGroundTime) <= COYOTE_TIME
-
 func handle_animations(direction):
 	if bouncing:
 		sprite.play("bounce")
@@ -179,6 +101,3 @@ func handle_animations(direction):
 func die():
 	sprite.play("death")
 	await sprite.animation_finished
-
-func play_jumping_animation():
-	sprite.play("jump")
